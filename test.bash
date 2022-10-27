ec6ba9cd967c7437fb6c13782719a2f2b83b3ef9
Satoshi Watanabe <s21C1141FX@s.chibakoudai.jp>
2022-10-27 13:48:26 +0900

    test

--- test.bash
+++ test.bash
@@ -1,5 +1,16 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Satoshi WatanabeSatoshi
+# SPDX- LICENCE- Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
 
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 15 ]
+[ "$res" = 0 ] && echo OK
+exit $res
